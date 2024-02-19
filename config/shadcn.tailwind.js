bc71fa8849b4342aacfcb136b7d831b0a81ad444
zhiye-tomoya <youhaochidecaiheping@gmail.com>
2024-02-19 23:58:09 +0900

    :sparkle:Add _navbar.html.erb

--- config/shadcn.tailwind.js
+++ config/shadcn.tailwind.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   darkMode: ["class"],
-  content: ["./public/*.html", "./app/helpers/**/*.rb", "./app/javascript/**/*.js", "./app/views/**/*.{erb,haml,html,slim}"],
+  content: ["./public/*.html", "./app/helpers/**/*.rb", "./app/javascript/**/*.js", "./app/views/**/*"],
   theme: {
     container: {
       center: true,
